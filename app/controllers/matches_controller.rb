c68f507badd5e5eca5d582e0a7b1e30901ba55b2
willieLOH <willie.loh@live.com>
2015-10-28 11:04:06 +0800

    capture keyup respond with 5-sec-poll

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,16 +1,22 @@
 class MatchesController < ApplicationController
   def show
+    @match = Match.find(params[:id])
   end
 
   def new
-    debugger
-  end
-
-  def create
-    debugger
+    if @match = Match.create(players: [params[:user], current_user.id])
+      redirect_to @match
+    else
+      redirect_to root_path
+    end
   end
 
   def update
-    debugger
+    @match = Match.find(params[:id])
+    if @match.whos_turn == current_user.id && @match.action(params[:move])
+      respond_to :js
+    else
+      render nothing: true
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027074935) do
+ActiveRecord::Schema.define(version: 20151027090126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151027074935) do
     t.integer  "winner"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "moves",      default: 0
   end
 
   create_table "users", force: :cascade do |t|
